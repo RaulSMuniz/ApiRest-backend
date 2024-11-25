21a3bd6e72ee90477427462e331f58abebeb5995
RaulSMuniz <raulanthonymuniz@gmail.com>
2024-11-25 16:48:20 -0300

    .

--- infraestrutura/connection.js
+++ infraestrutura/connection.js
@@ -5,8 +5,8 @@ const connection = mysql.createConnection(
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "3108",
-        database: "controle_atendimento"
+        password: "",
+        database: ""
     }
 );
 
